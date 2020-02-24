9fc56faece31fa7d846db7df502623dc28b732eb
twinh <twinhuang@qq.com>
2020-02-24 11:32:09 +0800

    feat: add import command

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as yargs from 'yargs';
 import * as npmlog from 'npmlog';
 import sync from '@gitsync/sync-command';
+import {default as imp} from '@gitsync/import-command';
 import commit from '@gitsync/commit-command';
 import postCommit from '@gitsync/post-commit-command';
 import postMerge from '@gitsync/post-merge-command';
@@ -9,21 +10,22 @@ import push from '@gitsync/push-command';
 import update from '@gitsync/update-command';
 import exec from '@gitsync/exec-command';
 import log from '@gitsync/log';
-import git from "git-cli-wrapper";
+import git from 'git-cli-wrapper';
 
 export default yargs
   .parserConfiguration({
-    'populate--': true
+    'populate--': true,
   })
   .options({
     'log-level': {
       group: 'Global Options:',
       describe: 'The level to display logs at',
       type: 'string',
-      default: 'info'
-    }
+      default: 'info',
+    },
   })
   .command(sync)
+  .command(imp)
   .command(commit)
   .command(update)
   .command(postCommit)
@@ -32,11 +34,11 @@ export default yargs
   .command(push)
   .command(exec)
   .middleware([
-    (argv) => {
+    argv => {
       // @ts-ignore
       npmlog.level = argv.logLevel;
       git.logger = log;
-    }
+    },
   ])
   .strict()
   .help();
